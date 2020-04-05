505c99eff0ed5d5285144284fcb19df11cccd7c8
th3dilli <th3dilli@gmx.at>
2020-04-05 18:49:11 +0200

    refactored folder structure for junit add junit add test class with dummy test

---
+++ core/src/test/java/com/mankomania/game/MankomaniaGameTest.java
@@ -0,0 +1,24 @@
+package com.mankomania.game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MankomaniaGameTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void render() {
+    }
+
+    @Test
+    public void dispose() {
+    }
+
+    @Test
+    public void testtest() {
+        Assert.assertTrue(true);
+    }
+}
