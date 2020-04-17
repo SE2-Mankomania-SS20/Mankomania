eeda966008f030fa09902a91111c3ba67077e356
soni4295 <soni.singh.95@hotmail.com>
2020-04-17 14:07:52 +0200

    MainGameScreen insert methods (create, render, doneLoading, dispose)

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/MainGameScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/MainGameScreen.java
@@ -1,24 +1,77 @@
 package com.mankomania.game.gamecore.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
+
 
 public class MainGameScreen extends ScreenAdapter {
+    public PerspectiveCamera cam;
+    public ModelBatch modelBatch;
+    public Environment environment;
+    public CameraInputController camController;
+    public AssetManager assets;
+    public boolean loading;
+    public ModelInstance instance;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+    public Model model;
+
 
     public void create() {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight,0.4f,0.4f,0.4f,1.0f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f,0.8f,-1.0f,-0-8f,-0.2f));
+        modelBatch = new ModelBatch();
 
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set (10.0f,80.0f,10.0f);
+        cam.lookAt(0,0,0);
+        cam.near = 3.0f;
+        cam.far = 300.0f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+        assets = new AssetManager();
+        assets.load("board.g3db", Model.class);
+        loading = true;
     }
 
     @Override
     public void render(float delta) {
+        if (loading && assets.update()) {
+            doneLoading();
+        }
+        Gdx.gl.glViewport(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+        camController.update();
 
     }
 
     private void doneLoading() {
-
+        Model board = assets.get("board.g3db", Model.class);
+        ModelInstance boardInstance = new ModelInstance(board);
+        instances.add(boardInstance);
+        loading = false;
     }
 
     @Override
     public void dispose() {
-
+        model.dispose();
+        modelBatch.dispose();
     }
 }
