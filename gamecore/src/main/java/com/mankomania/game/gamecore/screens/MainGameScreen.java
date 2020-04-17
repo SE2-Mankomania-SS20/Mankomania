b3d1dd3a8c85ea8a167408c561c6a899c3fd731b
soni4295 <soni.singh.95@hotmail.com>
2020-04-17 13:42:54 +0200

    create MainGameScreen class with methods

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/MainGameScreen.java
@@ -0,0 +1,24 @@
+package com.mankomania.game.gamecore.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class MainGameScreen extends ScreenAdapter {
+
+    public void create() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    private void doneLoading() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
