12e7a7a9b6507d96829a16e1eef4100d6a283312
Fabian Oraze <oraze.fabian@icloud.com>
2020-04-17 16:21:11 +0200

    implemented ChatScreen class with one textfield and one button to send it to server

--- gamecore/src/main/java/com/mankomania/game/gamecore/sreens/ChatScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/sreens/ChatScreen.java
@@ -2,18 +2,17 @@ package com.mankomania.game.gamecore.sreens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mankomania.game.core.network.ChatMessage;
 import com.mankomania.game.gamecore.MankomaniaGame;
+import com.mankomania.game.gamecore.client.NetworkClient;
+
 
 /*********************************
  Created by Fabian Oraze on 17.04.20
@@ -22,52 +21,64 @@ import com.mankomania.game.gamecore.MankomaniaGame;
 public class ChatScreen extends ScreenAdapter {
 
     MankomaniaGame game;
-    OrthographicCamera guiCam;
     Stage stage;
     TextField textField;
     TextButton sendButton;
     Skin skin;
+    NetworkClient client;
 
 
-    public ChatScreen(MankomaniaGame game) {
+    public ChatScreen(MankomaniaGame game, NetworkClient client) {
         this.game = game;
+        this.client = client;
         stage = new Stage();
         skin = new Skin(Gdx.files.internal("skin/terra-mother-ui.json"));
+        skin.getFont("font").getData().setScale(3, 3);
+
+        textField = new TextField("Enter chat", skin, "black");
+        textField.setColor(Color.BLACK);
+        textField.setPosition(40, Gdx.graphics.getHeight() - 100);
+        textField.setSize(Gdx.graphics.getWidth() - 20, 50);
+
+        sendButton = new TextButton("Send", skin, "black");
+        sendButton.setPosition(40, 100);
+        sendButton.background(skin.getDrawable("window-player-c"));
+
         Gdx.input.setInputProcessor(stage);
 
-        textField = new TextField("Chat", skin);
-        textField.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        textField.setSize(200, 200);
+
         stage.addActor(textField);
+        stage.addActor(sendButton);
+
+    }
+
+    public void draw() {
+
         textField.addListener(new ClickListener() {
             public void touchUp(InputEvent e, float x, float y, int point, int button) {
                 textField.setText("");
             }
         });
 
-        sendButton = new TextButton("Send", skin);
-        textField.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2 - 40);
-        textField.setSize(200, 200);
-        stage.addActor(sendButton);
         sendButton.addListener(new ClickListener() {
-            public void touchUp(InputEvent e, float x, float y, int point, int button) {
-                //TODO: send message to server
-                textField.setText("");
+            public void clicked(InputEvent e, float x, float y) {
+                sendMsgToServer(textField.getText());
             }
         });
 
-
     }
 
-    public void draw() {
-
+    public void sendMsgToServer(String msg) {
+        client.sendMsgToServer(new ChatMessage(msg));
     }
+
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        draw();
         stage.act(delta);
         stage.draw();
 
