b09f5dbfa8995d1496e1cdb216c4df86564c21c5
Felix Mitterer <felix.mitterer@posteo.de>
2020-04-18 18:30:10 +0200

    added a enum for field color and added a property for it to field

--- gamecore/src/main/java/com/mankomania/game/gamecore/fields/Field.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fields/Field.java
@@ -5,6 +5,8 @@ public class Field {
     private int nextId;
     private int optionalNextId;
 
+    private FieldColor color;
+
     private String text;
 
     // TODO: implement these field properties and add position on the field and player positions
@@ -14,11 +16,21 @@ public class Field {
 
     public Field() { }
 
-    public Field(int id, int nextId, int optNextId, String text) {
+    public Field(int id, int nextId, int optNextId, String color, String text) {
         this.id = id;
         this.nextId = nextId;
         this.optionalNextId = optNextId;
 
         this.text = text;
+
+        // load color through a string
+        switch (color.toLowerCase()) {
+            case "orange": this.color = FieldColor.ORANGE; break;
+            case "yellow": this.color = FieldColor.YELLOW; break;
+            case "blue": this.color = FieldColor.BLUE; break;
+            case "white": this.color = FieldColor.WHITE; break;
+            case "magenta": this.color = FieldColor.MAGENTA; break;
+            default: throw new IllegalArgumentException("the color given was empty or not parsable");
+        }
     }
 }
---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fields/FieldColor.java
@@ -0,0 +1,9 @@
+package com.mankomania.game.gamecore.fields;
+
+public enum FieldColor {
+    ORANGE,
+    YELLOW,
+    BLUE,
+    WHITE,
+    MAGENTA
+}
