e13231d7269027b142e5e666e407ef2e8d6f4b4f
Marija <marijaga@edu.aau.at>
2020-04-20 20:42:11 +0200

    Implemented PLAY button, BACK button and CHAT button for the screen transition

--- gamecore/src/main/java/com/mankomania/game/gamecore/MankomaniaGame.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/MankomaniaGame.java
@@ -1,10 +1,6 @@
 package com.mankomania.game.gamecore;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mankomania.game.gamecore.client.NetworkClient;
 import com.mankomania.game.gamecore.screens.LaunchScreen;
