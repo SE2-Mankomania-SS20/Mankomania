60ace15f851b4d5a9dc3b303a10ac59770bea885
Marija <marijaga@edu.aau.at>
2020-04-20 19:46:49 +0200

    Added methods to LobbyScreen

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/LobbyScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/LobbyScreen.java
@@ -1,5 +1,44 @@
 package com.mankomania.game.gamecore.screens;
 
-public class LobbyScreen {
-    //TODO
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+
+public class LobbyScreen extends ScreenAdapter {
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
