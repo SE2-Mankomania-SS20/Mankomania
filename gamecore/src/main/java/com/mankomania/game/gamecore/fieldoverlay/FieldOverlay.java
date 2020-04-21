d073fe21c1273adc463bfac13040141a6a8b2f75
Felix Mitterer <felix.mitterer@posteo.de>
2020-04-21 17:46:17 +0200

    small refactoring of (very large) render function in FieldOverlay

--- gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/FieldOverlay.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/FieldOverlay.java
@@ -52,31 +52,18 @@ public class FieldOverlay {
 
         // draw current center field
         float posX = -BOX_WIDTH / 2 - PADDING_LEFT;;
-        float posY = 0;
-
-        int tempCurrentSplitPosition = currentField.getSplitPosition();
-
-        if (tempCurrentSplitPosition == -1) {
-            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - MARGIN_TOP;
-        } else if (tempCurrentSplitPosition >= 0 && tempCurrentSplitPosition <= 3) {
-            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP;
-        } else if (tempCurrentSplitPosition >= 4 && tempCurrentSplitPosition <= 7) {
-            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP_ALTERNATE;
-        } else {
-            throw new IllegalArgumentException("split position cant be bigger than 5");
-        }
+        float posY = getFieldPosY(currentField.getSplitPosition());
 
         currentField.draw(batch, (int)posX, (int)posY, BOX_WIDTH, BOX_WIDTH);
         this.debugFont.draw(batch, "id = " + currentField.getId(), posX + 25, posY);
 
-
-        // draw right of center
+//         draw right of center
         int fieldNumberRight = 1; // how many fields is the current box shifted to the right of the center box
         for (int i = 1; i < FieldOverlayConfig.RENDER_DISTANCE_RIGHT; i++) {
             int nextId = currentCenterId - i; // handle < 0 and >= MAXSIZE
             if (nextId < 0) nextId = this.fieldOverlayData.getSize() + nextId;
             if (nextId >= this.fieldOverlayData.getSize()) nextId = nextId % this.fieldOverlayData.getSize();
-            currentField = fieldOverlayData.getById(nextId); // TODO: handle below 0 and above MAXSIZE
+            currentField = fieldOverlayData.getById(nextId);
 
             // calculate the x position of the current field with the information whether the way is split
             if (currentField.getSplitPosition() >= 0 && currentField.getSplitPosition() <= 3) {
@@ -85,10 +72,6 @@ public class FieldOverlay {
             } else { // if (currentField.getSplitPosition() >= 4 && currentField.getSplitPosition() <= 7) {
                 posX = -BOX_WIDTH / 2 + (fieldNumberRight * BOX_WIDTH) + ((fieldNumberRight + 1) * FieldOverlayConfig.MARGIN_BETWEEN);
                 fieldNumberRight++;
-//            } else { // no split field
-//                posX = -BOX_WIDTH / 2 + (fieldNumberRight * BOX_WIDTH) + ((fieldNumberRight + 1) * FieldOverlayConfig.MARGIN_BETWEEN);
-//                fieldNumberRight++;
-//            }
             }
 
             // need to substract 4 from the number that counts the X position, since we have 4 fields that are beneath the other one since they are split
@@ -97,22 +80,13 @@ public class FieldOverlay {
             }
 
             posX = posX - PADDING_LEFT;
-
-            if (currentField.getSplitPosition() == -1) {
-                posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - MARGIN_TOP;
-            } else if (currentField.getSplitPosition() >= 0 && currentField.getSplitPosition() <= 3) {
-                posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP;
-            } else if (currentField.getSplitPosition() >= 4 && currentField.getSplitPosition() <= 7) {
-                posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP_ALTERNATE;
-            } else {
-                throw new IllegalArgumentException("split position cant be bigger than 5");
-            }
+            posY = getFieldPosY(currentField.getSplitPosition());
 
             currentField.draw(batch, (int)posX, (int)posY, BOX_WIDTH, BOX_WIDTH);
-
             this.debugFont.draw(batch, "id = " + currentField.getId(), posX + 25, posY);
         }
     }
+
 
     /**
      * Frees all used resources
@@ -126,4 +100,25 @@ public class FieldOverlay {
 //    public void update(float delta) {
 //        // TODO: implement delta
 //    }
+
+    /**
+     * Calculates the Y position of the current field, paying attention to the field splits
+     * @param currentSplitPosition
+     * @return
+     */
+    private int getFieldPosY(int currentSplitPosition) {
+        int posY = 0;
+
+        if (currentSplitPosition == -1) {
+            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - MARGIN_TOP;
+        } else if (currentSplitPosition >= 0 && currentSplitPosition <= 3) {
+            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP;
+        } else if (currentSplitPosition >= 4 && currentSplitPosition <= 7) {
+            posY = (Gdx.graphics.getHeight() - BOX_WIDTH) - SPLIT_MARGIN_TOP_ALTERNATE;
+        } else {
+            throw new IllegalArgumentException("split position cant be bigger than 5");
+        }
+
+        return posY;
+    }
 }
