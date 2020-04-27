9beda9a8cb95ec33babce75e371da4f7f64e982a
Fabian Oraze <oraze.fabian@icloud.com>
2020-04-27 13:28:34 +0200

    created abstract screen class

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/AbstractScreen.java
@@ -0,0 +1,45 @@
+package com.mankomania.game.gamecore.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/*********************************
+ Created by Fabian Oraze on 27.04.20
+ *********************************/
+
+public abstract class AbstractScreen extends ScreenAdapter {
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
