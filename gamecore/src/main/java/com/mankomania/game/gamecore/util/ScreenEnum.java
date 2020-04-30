06fc32be0e517d23324daf8036bc6b9a6464c691
Fabian Oraze <oraze.fabian@icloud.com>
2020-04-30 11:23:06 +0200

    merge from develop

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/util/ScreenEnum.java
@@ -0,0 +1,36 @@
+package com.mankomania.game.gamecore.util;
+
+import com.mankomania.game.gamecore.MankomaniaGame;
+import com.mankomania.game.gamecore.client.NetworkClient;
+import com.mankomania.game.gamecore.screens.*;
+
+public enum ScreenEnum {
+
+    /**
+     * Holds all types of Screens with getScreen method
+     */
+    LAUNCH {
+        public AbstractScreen getScreen(Object... params) {
+            return new LaunchScreen();
+        }
+    },
+    LOBBY {
+        public AbstractScreen getScreen(Object... params) {
+            return new LobbyScreen();
+        }
+    },
+    CHAT {
+        public AbstractScreen getScreen(Object... params) {
+            return new ChatScreen((NetworkClient) params[0]);
+        }
+    },
+    MAIN_GAME {
+        public AbstractScreen getScreen(Object... params) {
+            return new MainGameScreen();
+        }
+    };
+
+    public abstract AbstractScreen getScreen(Object... params);
+
+
+}
