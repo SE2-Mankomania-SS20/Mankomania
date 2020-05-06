ecad2ac5e7788b3a74fe7cef3cf48107e467b32b
Felix Mitterer <felix.mitterer@posteo.de>
2020-05-06 12:54:31 +0200

    add functionality to hide/show functions and improved fading/scrolling in on hide/show

--- gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/FieldOverlayTextBox.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/FieldOverlayTextBox.java
@@ -1,11 +1,13 @@
 package com.mankomania.game.gamecore.fieldoverlay;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Align;
 
@@ -15,6 +17,13 @@ public class FieldOverlayTextBox {
     private Texture textBoxTextureBorder, textBoxTextureInner;
     private String currentText;
     private boolean isShowing;
+
+    private Interpolation interpolationIn = Interpolation.linear;
+    private float interpolationCurrent = 0;
+    private float interpolationDuration = 2; // TODO: move to config
+
+    private boolean isFadingIn = false;
+    private boolean isFadingOut = false;
 
     private BitmapFont textBoxFont;
     private GlyphLayout glyphLayout; // needed to calculate a strings width and height (for text rendering)
@@ -29,6 +38,7 @@ public class FieldOverlayTextBox {
         this.glyphLayout = new GlyphLayout(); // needed for calculating string dimensions for rendering, TODO: remove if not needed in future
 
         this.isShowing = true;
+        this.isFadingIn = true;
         this.currentText = "Kaufe 1 Aktie \"Kurzschluss-Versorungs-AG\" für 100.000€";
     }
 
@@ -40,14 +50,35 @@ public class FieldOverlayTextBox {
     public void render(SpriteBatch batch) {
         if (this.isShowing) {
 
+            // INTERPPOLATION BEGIN
+            if (this.isFadingIn) {
+                this.interpolationCurrent += Gdx.graphics.getDeltaTime();
+                if (this.interpolationCurrent >= interpolationDuration) {
+                    this.isFadingIn = false;
+                }
+
+            }
+            if (this.isFadingOut) {
+                this.interpolationCurrent -= Gdx.graphics.getDeltaTime();
+                if (this.interpolationCurrent <= 0) {
+                    this.isFadingOut = false;
+                }
+            }
+
+            float progress = Math.min(1f, interpolationCurrent / interpolationDuration); // 0 -> 1, 1 = showing, 0 = not showing
+            float percentVal = this.interpolationIn.apply(progress);
+//            int interpolatedPosX = (int)(TEXTBOX_POS_X + ((Gdx.graphics.getWidth() - TEXTBOX_POS_X) * percentVal));
+            int interpolatedPosX = (int)(Gdx.graphics.getWidth() - (Gdx.graphics.getWidth() - TEXTBOX_POS_X) * percentVal);
+            // == INTERPOLATION END ==
+
             // set our alpha value in the spritebatch's color
             Color color = batch.getColor();
             float oldAlpha = color.a; // remember old alpha value so we can reset it
 
             color.a = TEXTBOX_MAX_ALPHA;
             batch.setColor(color);
-            batch.draw(this.textBoxTextureInner, TEXTBOX_POS_X, TEXTBOX_POS_Y, TEXTBOX_WIDTH, TEXTBOX_HEIGHT);
-            batch.draw(this.textBoxTextureBorder, TEXTBOX_POS_X, TEXTBOX_POS_Y, TEXTBOX_WIDTH, TEXTBOX_HEIGHT);
+            batch.draw(this.textBoxTextureInner, interpolatedPosX, TEXTBOX_POS_Y, TEXTBOX_WIDTH, TEXTBOX_HEIGHT);
+            batch.draw(this.textBoxTextureBorder, interpolatedPosX, TEXTBOX_POS_Y, TEXTBOX_WIDTH, TEXTBOX_HEIGHT);
 
             // reset to the old alpha value
             color.a = oldAlpha;
@@ -67,6 +98,7 @@ public class FieldOverlayTextBox {
     public void show() {
         // TODO: add fading
         this.isShowing = true;
+        this.isFadingIn = true;
     }
 
     /**
@@ -74,7 +106,8 @@ public class FieldOverlayTextBox {
      */
     public void hide() {
         // TODO: add fading
-        this.isShowing = false;
+//        this.isShowing = false;
+        this.isFadingOut = true;
     }
 
     public void dispose() {
