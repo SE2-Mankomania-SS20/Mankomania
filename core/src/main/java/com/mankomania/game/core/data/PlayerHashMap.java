f9dd502d33f916cb3299f918209ee641cc75f2cc
Fabian Oraze <oraze.fabian@icloud.com>
2020-05-08 22:10:52 +0200

    implemented base structure to gameData class --> now loads json data properly

---
+++ core/src/main/java/com/mankomania/game/core/data/PlayerHashMap.java
@@ -0,0 +1,17 @@
+package com.mankomania.game.core.data;
+
+import com.mankomania.game.core.player.Player;
+
+import java.util.HashMap;
+
+/*********************************
+ Created by Fabian Oraze on 08.05.20
+ *********************************/
+
+//TODO: make custom hashMap
+
+public class PlayerHashMap extends HashMap<Integer, Player> {
+
+
+
+}
