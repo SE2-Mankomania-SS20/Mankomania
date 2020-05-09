3049cfa45f881ea4befbf5d8231552bb22460726
Felix Mitterer <felix.mitterer@posteo.de>
2020-05-09 22:03:29 +0200

    implemented a column class and a colum data class. this way rendering will be easier to read and understand.

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/fielddata/FieldOverlayFieldColumn.java
@@ -0,0 +1,91 @@
+package com.mankomania.game.gamecore.fieldoverlay.fielddata;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mankomania.game.gamecore.fieldoverlay.FieldOverlayField;
+
+/**
+ * Class that represents a column in the field overlay and stores it's id and the fields that are part of it.
+ */
+public class FieldOverlayFieldColumn {
+    private int columnId;
+    private boolean hasDoubleRow, isEmpty;
+    private FieldOverlayField fieldTop;
+    private FieldOverlayField fieldMid;
+    private FieldOverlayField fieldBot;
+
+    private float positionX;
+
+    public FieldOverlayFieldColumn(int columnId) {
+        this.columnId = columnId;
+        this.isEmpty = true;
+    }
+
+    public FieldOverlayFieldColumn(int columnId, FieldOverlayField middleField) {
+        this.columnId = columnId;
+        this.fieldMid = middleField;
+
+        this.hasDoubleRow = false;
+        this.isEmpty = false;
+    }
+
+    public FieldOverlayFieldColumn(int columnId, FieldOverlayField topField, FieldOverlayField bottomField) {
+        this.columnId = columnId;
+        this.fieldTop = topField;
+        this.fieldBot = bottomField;
+
+        this.hasDoubleRow = true;
+        this.isEmpty = false;
+    }
+
+    /**
+     * Renders all fields of this column.
+     * @param batch SpriteBatch used for rendering
+     */
+    public void renderFields(SpriteBatch batch) {
+        if (!this.isEmpty) {
+            if (this.hasDoubleRow) {
+                this.fieldTop.draw(batch);
+                this.fieldBot.draw(batch);
+            } else {
+                this.fieldMid.draw(batch);
+            }
+        }
+    }
+
+    /**
+     * Move the column this amount.
+     * @param value how much it should move
+     */
+    public void movePositionX(float value) {
+        float newPosition = this.positionX + value;
+
+        this.setPositionX(newPosition);
+    }
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(float positionX) {
+        this.positionX = positionX;
+
+        if (!this.isEmpty) {
+            if (this.hasDoubleRow) {
+                this.fieldTop.setCurrentPosX(positionX);
+                this.fieldBot.setCurrentPosX(positionX);
+            } else {
+                this.fieldMid.setCurrentPosX(positionX);
+            }
+        }
+    }
+
+    public int getColumnId() {
+        return columnId;
+    }
+
+    @Override
+    public String toString() {
+        return "Column (" + this.columnId + "): fields count = " +
+                (isEmpty ? "0" : (hasDoubleRow ? "2" : "1"));
+    }
+}
---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/fieldoverlay/fielddata/FieldOverlayFieldColumnData.java
@@ -0,0 +1,41 @@
+package com.mankomania.game.gamecore.fieldoverlay.fielddata;
+
+import com.mankomania.game.gamecore.fieldoverlay.FieldOverlayField;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Class that loads and holds all of the columns.
+ */
+public class FieldOverlayFieldColumnData {
+    private FieldOverlayFieldColumn[] columns;
+    public FieldOverlayFieldColumnData() {
+
+    }
+
+    public void create(HashMap<Integer, ArrayList<FieldOverlayField>> fieldMap) {
+        this.columns = new FieldOverlayFieldColumn[132];
+
+        // iterate over all columns existing (field count * 2)
+        for (int i = 0; i < 132; i++) {
+            FieldOverlayFieldColumn columnToAdd;
+            if (fieldMap.containsKey(i)) {
+                ArrayList<FieldOverlayField> fieldsInColumn = fieldMap.get(i);
+
+                if (fieldsInColumn.size() == 1) {
+                    columnToAdd = new FieldOverlayFieldColumn(i, fieldsInColumn.get(0));
+                } else {
+                    columnToAdd = new FieldOverlayFieldColumn(i, fieldsInColumn.get(0), fieldsInColumn.get(1));
+                }
+            } else {
+                columnToAdd = new FieldOverlayFieldColumn(i);
+            }
+            this.columns[i] = columnToAdd;
+        }
+    }
+
+    public FieldOverlayFieldColumn getColumnById(int id) {
+        return this.columns[id];
+    }
+}
