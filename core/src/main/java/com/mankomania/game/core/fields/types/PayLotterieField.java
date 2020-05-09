28197a189b94422f8e30420f2ffc4591fbe226b6
Felix Mitterer <felix.mitterer@posteo.de>
2020-05-09 18:31:59 +0200

    added field class for field type PayLotterie

---
+++ core/src/main/java/com/mankomania/game/core/fields/types/PayLotterieField.java
@@ -0,0 +1,18 @@
+package com.mankomania.game.core.fields.types;
+
+import com.mankomania.game.core.fields.FieldColor;
+import com.mankomania.game.core.fields.Position3;
+
+public class PayLotterieField extends Field {
+    private int amountToPay;
+
+    public PayLotterieField(Position3[] positions, int nextField, int optionalNextField, int previousField, String text, FieldColor color, int amount) {
+        super(positions, nextField, optionalNextField, previousField, text, color);
+
+        this.amountToPay = amount;
+    }
+
+    public int getAmountToPay() {
+        return amountToPay;
+    }
+}
