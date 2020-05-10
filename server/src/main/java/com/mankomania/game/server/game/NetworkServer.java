ae493ac4bfeb5d4273eb77a8511362beace2ecc0
Felix Mitterer <felix.mitterer@posteo.de>
2020-05-10 22:47:45 +0200

    changed command line output to real logger. added some more verbose logging output.

--- server/src/main/java/com/mankomania/game/server/game/NetworkServer.java
+++ server/src/main/java/com/mankomania/game/server/game/NetworkServer.java
@@ -1,8 +1,10 @@
 package com.mankomania.game.server.game;
 
 import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.FrameworkMessage;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
 import com.mankomania.game.core.data.GameData;
 import com.mankomania.game.core.network.KryoHelper;
 import com.mankomania.game.core.network.messages.ChatMessage;
@@ -25,6 +27,8 @@ public class NetworkServer {
 
 
     public NetworkServer() throws IOException {
+        Log.info("Starting server, listening on port " + NetworkConstants.TCP_PORT);
+
         server = new Server();
         //call helper class to register classes
         KryoHelper.registerClasses(server.getKryo());
@@ -35,33 +39,44 @@ public class NetworkServer {
 
         serverData = new ServerData();
 
-
-
         server.addListener(new Listener() {
             @Override
             public void received(Connection connection, Object object) {
+                // keep KeepAlive messages from spamming the log
+                if (!(object instanceof FrameworkMessage.KeepAlive)) {
+                    Log.info("Message received from " + connection.toString() +
+                            " at endpoint " + connection.getRemoteAddressTCP().toString() +
+                            "; message class = " + object.getClass().getTypeName());
+                }
+
+                /* ==== PlayerDisconnected ==== */
                 if (object instanceof PlayerDisconnected) {
                     connection.close();
                 }
 
+                /* ==== ChatMessage ==== */
                 if (object instanceof ChatMessage) {
                     ChatMessage request = (ChatMessage) object;
                     request.text = "Player " + connection.getID() + ": " + request.text;
-                    System.out.println(request.text);
+
+                    Log.info("Chat message from " + connection.toString() + ": " + request.text);
 
                     server.sendToAllTCP(request);
                 }
 
+                /* ==== PlayerGameReady ==== */
                 if (object instanceof PlayerGameReady) {
                     PlayerGameReady state = (PlayerGameReady) object;
                     boolean ready = state.playerReady;
 
                     serverData.playerReady(connection, ready);
-                    System.out.println("Player " + connection.getID() + " is ready!");
+                    Log.info(connection.toString() + " is ready!");
 
                     //TODO: send notification to all TCPs that player is ready
 
+                    // if all players are ready, start the game and notify all players
                     if (serverData.checkForStart()) {
+                        Log.info("Game will be started now (all player are ready, player count: " + serverData.initPlayerList().size() + ")");
                         state.gameReady = true;
                         server.sendToAllTCP(state);
                         InitPlayers listIDs = new InitPlayers();
@@ -83,39 +98,46 @@ public class NetworkServer {
 
             @Override
             public void connected(Connection connection) {
+                Log.info("Player connected: " + connection.toString() +
+                        " from endpoint " + connection.getRemoteAddressTCP().toString());
+
                 if (serverData.connectPlayer(connection)) {
-                    System.out.println("Player has connected");
+                    Log.info("Player (" + connection.toString() + ") accepted on server.");
                 } else {
-                    System.err.println("Player could not connect!");
+                    Log.error("Player (" + connection.toString() + ") could not connect! Lobby already full? Sending DisconnectPlayerMessage...");
+
                     DisconnectPlayer disCon = new DisconnectPlayer();
                     disCon.errTxt = "Server already full!";
                     server.sendToTCP(connection.getID(), disCon);
-
-
                 }
             }
 
             @Override
             public void disconnected(Connection connection) {
+                Log.info("Player disconnected: " + connection.toString() +
+                        " from endpoint " + connection.getRemoteAddressTCP().toString());
+
                 DisconnectPlayer disCon = new DisconnectPlayer();
                 disCon.errTxt = "Client disconnected unexpectedly";
                 server.sendToTCP(connection.getID(), disCon);
                 serverData.disconnectPlayer(connection);
+                // TODO: send notification to all clients
                 super.disconnected(connection);
             }
         });
-        System.out.println("Server is ready...");
+
+        Log.info("Server ready and listening on port " + NetworkConstants.TCP_PORT + "!");
     }
 
     public void processCommand(String command) {
         switch (command) {
             case "exit": {
                 server.stop();
-                System.out.println("Server is shutting down...");
+                Log.info("Console command: Server is shutting down...");
                 System.exit(0);
             }
             default: {
-                System.out.println("Command \"" + command + "\" not recognized.");
+                Log.info("Console command: Command \"" + command + "\" not recognized.");
                 break;
             }
         }
