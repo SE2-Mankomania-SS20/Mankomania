173d54667106e056634a4f954e794acdf104b160
soni4295 <soni.singh.95@hotmail.com>
2020-05-16 18:13:12 +0200

    RouletteMinigame -> set skin and font size of skin

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/RouletteMinigameScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/RouletteMinigameScreen.java
@@ -1,11 +1,30 @@
 package com.mankomania.game.gamecore.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 public class RouletteMinigameScreen extends AbstractScreen {
+    private Stage stage;
+    private Skin skin, skin1, skin2;
+
+
 
     public RouletteMinigameScreen () {
+        //set skin
+        skin = new Skin(Gdx.files.internal("skin/terra-mother-ui.json"));
+        skin1 = new Skin(Gdx.files.internal("skin/uiskin.json"));
+        skin2 = new Skin(Gdx.files.internal("skin/terra-mother-ui.json"));
+
+        //set font size of skin
+        skin.getFont("font").getData().setScale(5, 5);
+        skin1.getFont("default-font").getData().setScale(3, 3);
+        skin2.getFont("font").getData().setScale(3, 3);
+
+
 
     }
-    
+
     @Override
     public void render(float delta) {
         super.render(delta);
