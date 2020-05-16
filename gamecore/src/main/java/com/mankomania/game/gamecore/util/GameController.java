7c9f5820c655617d8025c707ac771ac2a1fc3cfa
Fabian Oraze <oraze.fabian@icloud.com>
2020-05-16 12:11:45 +0200

    added gameController class to gameCore module, should be a singleton which provides access to the persistent Game object, throughout the lifecycle of the game

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/util/GameController.java
@@ -0,0 +1,11 @@
+package com.mankomania.game.gamecore.util;
+
+/*********************************
+ Created by Fabian Oraze on 14.05.20
+ *********************************/
+
+public class GameController {
+
+
+
+}
