d2c9c13b4a55c8df87dbcc6c64e5f71338bd9fb0
Fabian Oraze <oraze.fabian@icloud.com>
2020-05-16 12:19:55 +0200

    implemented basic GameController logic

--- gamecore/src/main/java/com/mankomania/game/gamecore/util/GameController.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/util/GameController.java
@@ -1,11 +1,42 @@
 package com.mankomania.game.gamecore.util;
 
+import com.badlogic.gdx.Game;
+import com.mankomania.game.gamecore.MankomaniaGame;
+
 /*********************************
  Created by Fabian Oraze on 14.05.20
  *********************************/
 
 public class GameController {
 
+    /**
+     * Controller class which provides access to the Game object via a singleton pattern
+     */
+
+    private static GameController instance;
+    private MankomaniaGame game;
+
+    private GameController() {
+        super();
+    }
+
+
+    /**
+     * should be called once when game starts
+     *
+     * @param game object of libGdx game type
+     */
+    public static void initialize(MankomaniaGame game) {
+        if (instance == null) {
+            instance = new GameController();
+        }
+        instance.game = game;
+    }
+
+
+    public static MankomaniaGame getGame() {
+        return instance.game;
+    }
 
 
 }
