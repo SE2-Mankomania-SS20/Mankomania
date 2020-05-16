d078c45ba86273bdff7422656c89a8345c669190
soni4295 <soni.singh.95@hotmail.com>
2020-05-16 18:09:24 +0200

    RouletteMinigame -> add Override methods and create constuctor

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/RouletteMinigameScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/RouletteMinigameScreen.java
@@ -1,4 +1,37 @@
 package com.mankomania.game.gamecore.screens;
 
-public class RouletteMinigameScreen {
+public class RouletteMinigameScreen extends AbstractScreen {
+
+    public RouletteMinigameScreen () {
+
+    }
+    
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
