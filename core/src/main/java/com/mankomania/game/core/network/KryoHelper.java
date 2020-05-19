8237da8c430d61df39d8160752edc6627d68f01f
th3dilli <th3dilli@gmx.at>
2020-05-19 13:13:11 +0200

    refactored PlayerDisconnected and KryoHelper imports

--- core/src/main/java/com/mankomania/game/core/network/KryoHelper.java
+++ core/src/main/java/com/mankomania/game/core/network/KryoHelper.java
@@ -2,21 +2,11 @@ package com.mankomania.game.core.network;
 
 import com.badlogic.gdx.graphics.Color;
 import com.esotericsoftware.kryo.Kryo;
-import com.mankomania.game.core.network.messages.ChatMessage;
-import com.mankomania.game.core.network.messages.PlayerGameReady;
-import com.mankomania.game.core.network.messages.clienttoserver.PlayerDisconnected;
-import com.mankomania.game.core.network.messages.servertoclient.Notification;
-import com.mankomania.game.core.network.messages.servertoclient.PlayerConnected;
-import com.mankomania.game.core.network.messages.clienttoserver.baseturn.DiceResultMessage;
-import com.mankomania.game.core.network.messages.clienttoserver.baseturn.IntersectionSelectedMessage;
-import com.mankomania.game.core.network.messages.servertoclient.GameStartedMessage;
-import com.mankomania.game.core.network.messages.servertoclient.InitPlayers;
-import com.mankomania.game.core.network.messages.servertoclient.baseturn.MovePlayerOverLotteryMessage;
-import com.mankomania.game.core.network.messages.servertoclient.baseturn.MovePlayerToFieldAfterIntersectionMessage;
-import com.mankomania.game.core.network.messages.servertoclient.baseturn.MovePlayerToFieldMessage;
-import com.mankomania.game.core.network.messages.servertoclient.baseturn.MovePlayerToIntersectionMessage;
-import com.mankomania.game.core.network.messages.servertoclient.baseturn.PlayerCanRollDiceMessage;
-
+import com.mankomania.game.core.network.messages.*;
+import com.mankomania.game.core.network.messages.clienttoserver.*;
+import com.mankomania.game.core.network.messages.servertoclient.*;
+import com.mankomania.game.core.network.messages.clienttoserver.baseturn.*;
+import com.mankomania.game.core.network.messages.servertoclient.baseturn.*;
 /*
  Created by Fabian Oraze on 06.05.20
  */
@@ -26,13 +16,12 @@ public class KryoHelper {
     public static void registerClasses(Kryo kryo) {
 
         kryo.register(ChatMessage.class);
-        kryo.register(PlayerGameReady.class);
+        kryo.register(PlayerReady.class);
         kryo.register(InitPlayers.class);
         kryo.register(java.util.ArrayList.class);
         kryo.register(PlayerConnected.class);
-        kryo.register(PlayerDisconnected.class);
 
-        kryo.register(GameStartedMessage.class);
+        kryo.register(StartGame.class);
         kryo.register(PlayerCanRollDiceMessage.class);
         kryo.register(DiceResultMessage.class);
         kryo.register(MovePlayerToFieldMessage.class);
