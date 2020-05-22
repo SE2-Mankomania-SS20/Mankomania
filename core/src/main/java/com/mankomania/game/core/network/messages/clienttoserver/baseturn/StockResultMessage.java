96cb59a50c4cb864c95d74bffac62cbab83d906c
Ajdin Memić <ajdinmemic99@outlook.com>
2020-05-22 19:41:46 +0200

    Server: StockResultMessage Klasse hinzugefuegt

---
+++ core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/baseturn/StockResultMessage.java
@@ -0,0 +1,30 @@
+package com.mankomania.game.core.network.messages.clienttoserver.baseturn;
+
+public class StockResultMessage {
+    private int playerId;
+    private int StockResult;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getStockResult() {
+        return StockResult;
+    }
+
+    public void setStockResult(int stockResult) {
+        this.StockResult = stockResult;
+    }
+
+    public static StockResultMessage createStockResultMessage(int playerId, int StockResult) {
+        StockResultMessage message = new StockResultMessage();
+        message.setPlayerId(playerId);
+        message.setStockResult(StockResult);
+
+        return message;
+    }
+}
