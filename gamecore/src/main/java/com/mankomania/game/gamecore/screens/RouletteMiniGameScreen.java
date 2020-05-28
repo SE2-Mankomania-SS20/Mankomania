91b7bfac392aef89bda314ddd1469310890034ea
soni4295 <soni.singh.95@hotmail.com>
2020-05-28 14:10:12 +0200

    RouletteMiniGameScreen -> updating RouletteMiniGameScreen.class, remove code smells

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/RouletteMiniGameScreen.java
@@ -0,0 +1,407 @@
+package com.mankomania.game.gamecore.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Timer;
+import com.mankomania.game.core.network.messages.servertoclient.minigames.RouletteResultMessage;
+import com.mankomania.game.gamecore.MankomaniaGame;
+import com.mankomania.game.gamecore.client.RouletteClient;
+import com.mankomania.game.gamecore.minigames.RouletteResultOfPlayers;
+import com.mankomania.game.gamecore.util.Screen;
+import com.mankomania.game.gamecore.util.ScreenManager;
+
+import java.util.ArrayList;
+
+public class RouletteMiniGameScreen extends AbstractScreen {
+
+    private RouletteClient rouletteClient = RouletteClient.getInstance();
+    private Stage stage;
+    private Skin skin;
+    private Skin skin1;
+    private Skin skin2;
+    private Texture texturePointer;
+    private Texture   textureWheel;
+    private Image imagePointer, imageWheel;
+    private Label bet1;
+    private Label bet2;
+    private Label bet3;
+    private TextField textFieldEnteredNumber;
+    private TextField textFieldInputPlayer;
+    private TextField textFieldResultWheel;
+    private TextButton textButton1;
+    private TextButton textButton2;
+    private TextButton textButton3;
+    private TextButton textButton4;
+    private TextButton textButton5;
+    private TextButton textButtonReady;
+    private TextButton textButtonCheck;
+    private Table tableMain;
+    private Table table1;
+    private Table table2;
+    private boolean tb1;
+    private boolean tb2;
+    private boolean tb3;
+    private boolean tb4;
+    private boolean tb5;
+    private boolean tb6;
+    private int bettingOpportunity;
+    private RouletteResultOfPlayers rouletteResultOfPlayers = new RouletteResultOfPlayers();
+    private Dialog dialogWaitingResult;
+    private final String blackString = "black";
+    private final String defaultString = "default";
+
+    public RouletteMiniGameScreen() {
+        //set skin
+        skin = new Skin(Gdx.files.internal("skin/terra-mother-ui.json"));
+        skin1 = new Skin(Gdx.files.internal("skin/uiskin.json"));
+        skin2 = new Skin(Gdx.files.internal("skin/terra-mother-ui.json"));
+
+        //set font size of skin
+        skin.getFont("font").getData().setScale(5, 5);
+        skin1.getFont("default-font").getData().setScale(3, 3);
+        skin2.getFont("font").getData().setScale(3, 3);
+
+        //image for pointer and wheel
+        texturePointer = new Texture(Gdx.files.internal("Minigame_Roulette/PointerRoulette.png"));
+        imagePointer = new Image(texturePointer);
+        textureWheel = new Texture(Gdx.files.internal("Minigame_Roulette/WheelRoulette.png"));
+        imageWheel = new Image(textureWheel);
+
+        //1.Einsatz 5000€ - Gewinn 150000€ + Textfield
+        bet1 = new Label("1. Einsatz 5.000 \u20AC - Gewinn 150.000 \u20AC", skin, blackString);
+        textFieldEnteredNumber = new TextField("", skin2, blackString);
+        textFieldEnteredNumber.setColor(Color.BLACK);
+        textFieldEnteredNumber.setAlignment(Align.left); //Text in center
+        textButtonCheck = new TextButton("Check", skin1, "default");
+
+        //2.Einsatz 20000€ - Gewinn 100000€ + Checkbox
+        bet2 = new Label("2. Einsatz 20.000 \u20AC - Gewinn 100.000 \u20AC", skin, blackString);
+        textButton1 = new TextButton("1-12", skin1, defaultString);
+        textButton2 = new TextButton("13-24", skin1, defaultString);
+        textButton3 = new TextButton("25-36", skin1, defaultString);
+
+        //3.Einsatz 50000€ - Gewinn 80000€ + Checkbox
+        bet3 = new Label("3. Einsatz 50.000 \u20AC - Gewinn 80.000 \u20AC", skin, blackString);
+        textButton4 = new TextButton("Rot", skin1, defaultString);
+        textButton5 = new TextButton("Schwarz", skin1, defaultString);
+
+        //input from player viewed in a textfield
+        textFieldInputPlayer = new TextField("", skin2, blackString);
+        textFieldInputPlayer.setColor(Color.BLACK);
+        textFieldInputPlayer.setAlignment(Align.center);
+
+        //result viewed in a textfield
+        textFieldResultWheel = new TextField("", skin2, blackString);
+        textFieldResultWheel.setColor(Color.BLACK);
+        textFieldResultWheel.setAlignment(Align.center);
+
+        textButtonReady = new TextButton("READY", skin2, "default");
+
+        stage = new Stage();
+        table1 = new Table();
+        table1.setFillParent(false);
+        table1.setWidth(Gdx.graphics.getWidth());
+        table1.setHeight(Gdx.graphics.getHeight());
+
+        table2 = new Table();
+        table2.setFillParent(false);
+        table2.setWidth(Gdx.graphics.getWidth());
+        table2.setHeight(Gdx.graphics.getHeight());
+
+        table1.add(imagePointer).width(150).height(150).padTop(50);
+        table1.row();
+        table1.add(imageWheel).width(Gdx.graphics.getWidth() / 2f - 200).height(Gdx.graphics.getWidth() / 2f - 200);
+        table1.row();
+
+        table2.add(bet1).width(800).height(120).padTop(50);
+        table2.row();
+        table2.add(textFieldEnteredNumber).width(700).height(120);
+        table2.add(textButtonCheck).padRight(380).width(200).height(100);
+        table2.row();
+        table2.add(bet2).width(800).height(120);
+        table2.row();
+        table2.add(textButton1).width(250).height(120);
+        table2.row();
+        table2.add(textButton2).width(250).height(120);
+        table2.row();
+        table2.add(textButton3).width(250).height(120);
+        table2.row();
+        table2.add(bet3).width(800).height(120);
+        table2.row();
+        table2.add(textButton4).width(400).height(120);
+        table2.add(textButton5).width(400).height(120);
+        table2.row();
+        table2.add(textFieldInputPlayer).width(400).height(100);
+        table2.add(textFieldResultWheel).width(400).height(100);
+        table2.row();
+        table2.add(textButtonReady).width(500).height(100);
+        table2.row();
+
+        //join table1 and table2 to tableMain
+        tableMain = new Table();
+        tableMain.setFillParent(true);
+        tableMain.add(table1);
+        tableMain.add(table2);
+
+        tb1 = false;
+        tb2 = false;
+        tb3 = false;
+        tb4 = false;
+        tb5 = false;
+        tb6 = false;
+
+        Gdx.input.setInputProcessor(stage);
+
+        textFieldEnteredNumber.addListener(new ClickListener() {
+            @Override
+            public void touchUp(InputEvent e, float x, float y, int point, int button) {
+                textFieldEnteredNumber.setText("");
+                textButton1.setColor(Color.GRAY);
+                textButton2.setColor(Color.GRAY);
+                textButton3.setColor(Color.GRAY);
+                textButton4.setColor(Color.GRAY);
+                textButton5.setColor(Color.GRAY);
+            }
+        });
+
+        // update ui callback
+        rouletteClient.setCallback(() -> {
+            System.out.println("Received Results");
+            updateUI();
+        });
+
+        textButtonReady.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                int amountBet;
+                switch (bettingOpportunity) {
+                    case 1: amountBet = 5000; break;
+                    case 2: amountBet = 20000; break;
+                    case 3: amountBet = 20000; break;
+                    case 4: amountBet = 20000; break;
+                    case 5: amountBet = 50000; break;
+                    case 6: amountBet = 50000; break;
+                    default: amountBet = 0; break;
+                }
+                int betOptions = chooseBetButton();
+                //sendRouletteStackMessage -> client input send to server
+                MankomaniaGame.getMankomaniaGame().getClient().getMessageHandler().sendRouletteStackMessage(betOptions, amountBet);
+
+                /*
+                Spieler der Einsätze gesetzt hat, soll einen Dialog am Bildschirm erhalten. Bis alle Ergebnisse am Client sind.
+                 */
+
+                Skin uiSkin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+                uiSkin.getFont("default-font").getData().setScale(10, 10);
+                dialogWaitingResult = new Dialog("waiting for Result..", uiSkin, "dialog") {
+                    public void result(Object obj) {
+                        System.out.println("result " + obj);
+                    }
+                };
+                dialogWaitingResult.show(stage);
+            }
+        });
+
+        textButtonCheck.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent e, float x, float y) {
+                String enteredRouletteNumber = textFieldEnteredNumber.getText();
+                int numberInString = Integer.parseInt(enteredRouletteNumber);
+
+                try {
+                    if (isNumeric(enteredRouletteNumber)) {
+                        if (numberInString >= 1 && numberInString <= 36) {
+                            textFieldInputPlayer.setText(textFieldEnteredNumber.getText());
+                            bettingOpportunity = 1;
+                            tb1 = true;
+                        } else {
+                            textFieldEnteredNumber.setText("1-36");
+                        }
+                    }
+
+                } catch (NumberFormatException ex) {
+                    ex.printStackTrace();
+                    textFieldEnteredNumber.setText("1-36");
+                }
+            }
+        });
+
+        //if player choose button 1 the other are disabled
+        textButton1.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                bettingOpportunity = 2;
+                tb1 = false;
+                tb2 = true;
+                tb3 = false;
+                tb4 = false;
+                tb5 = false;
+                tb6 = false;
+                textButton1.setColor(Color.RED);
+                textButton2.setColor(Color.GRAY);
+                textButton3.setColor(Color.GRAY);
+                textButton4.setColor(Color.GRAY);
+                textButton5.setColor(Color.GRAY);
+                textFieldInputPlayer.setText("1-12");
+
+            }
+        });
+
+        textButton2.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                bettingOpportunity = 3;
+                tb1 = false;
+                tb2 = false;
+                tb3 = true;
+                tb4 = false;
+                tb5 = false;
+                tb6 = false;
+                textButton1.setColor(Color.GRAY);
+                textButton2.setColor(Color.RED);
+                textButton3.setColor(Color.GRAY);
+                textButton4.setColor(Color.GRAY);
+                textButton5.setColor(Color.GRAY);
+                textFieldInputPlayer.setText("13-24");
+            }
+        });
+
+        textButton3.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                bettingOpportunity = 4;
+                tb1 = false;
+                tb2 = false;
+                tb3 = false;
+                tb4 = true;
+                tb5 = false;
+                tb6 = false;
+                textButton1.setColor(Color.GRAY);
+                textButton2.setColor(Color.GRAY);
+                textButton3.setColor(Color.RED);
+                textButton4.setColor(Color.GRAY);
+                textButton5.setColor(Color.GRAY);
+                textFieldInputPlayer.setText("25-36");
+            }
+        });
+
+        textButton4.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                bettingOpportunity = 5;
+                tb1 = false;
+                tb2 = false;
+                tb3 = false;
+                tb4 = false;
+                tb5 = true;
+                tb6 = false;
+                textButton1.setColor(Color.GRAY);
+                textButton2.setColor(Color.GRAY);
+                textButton3.setColor(Color.GRAY);
+                textButton4.setColor(Color.RED);
+                textButton5.setColor(Color.GRAY);
+                textFieldInputPlayer.setText("rot");
+            }
+        });
+
+        textButton5.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                bettingOpportunity = 6;
+                tb1 = false;
+                tb2 = false;
+                tb3 = false;
+                tb4 = false;
+                tb5 = false;
+                tb6 = true;
+                textButton1.setColor(Color.GRAY);
+                textButton2.setColor(Color.GRAY);
+                textButton3.setColor(Color.GRAY);
+                textButton4.setColor(Color.GRAY);
+                textButton5.setColor(Color.RED);
+                textFieldInputPlayer.setText("schwarz");
+            }
+        });
+        stage.addActor(tableMain);
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public int chooseBetButton() {
+        int rouletteValue = 0;
+        if (tb1) {
+            String enteredNumberInTextField = textFieldEnteredNumber.getText();
+            int numberInInt = Integer.parseInt(enteredNumberInTextField);
+            rouletteValue = numberInInt;
+        } else if (tb2) {
+            rouletteValue = 37;
+        } else if (tb3) {
+            rouletteValue = 38;
+        } else if (tb4) {
+            rouletteValue = 39;
+        } else if (tb5) {
+            rouletteValue = 40;
+        } else if (tb6) {
+            rouletteValue = 41;
+        }
+        return rouletteValue;
+    }
+
+    public void updateUI() {
+
+        // set result field
+        textFieldResultWheel.setText(MankomaniaGame.getMankomaniaGame().getGameData().getArrayPlayerInformation().get(0).getResultOfRouletteWheel());
+
+        ArrayList<RouletteResultMessage> results = MankomaniaGame.getMankomaniaGame().getGameData().getArrayPlayerInformation();
+
+        float delayInSeconds = 10f; //2 Sekunden
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                dialogWaitingResult.hide();
+                tableMain.clear();
+                Table tabelResult = rouletteResultOfPlayers.createRouletteResultOfPlayers(results);
+                tableMain.add(tabelResult);
+
+            }
+        }, delayInSeconds);
+
+        float delayInSecondsTable = 20f; //2 Sekunden
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                ScreenManager.getInstance().switchScreen(Screen.MAIN_GAME);
+            }
+        }, delayInSecondsTable);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        stage.act(delta);
+        stage.draw();
+    }
+
+}
