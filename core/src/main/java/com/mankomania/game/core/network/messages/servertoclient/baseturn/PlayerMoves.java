6b3a5d7927713937d0ae5720cbae9524f03b948f
Th3Dilli <dilli.1822@gmx.at>
2020-05-30 17:16:44 +0200

    add equal and hash function for tests with new player move

--- core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/baseturn/PlayerMoves.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/baseturn/PlayerMoves.java
@@ -2,6 +2,8 @@ package com.mankomania.game.core.network.messages.servertoclient.baseturn;
 
 import com.badlogic.gdx.utils.IntArray;
 
+import java.util.Objects;
+
 /**
  * Just moves the given player to the given field.
  */
@@ -24,4 +26,20 @@ public class PlayerMoves {
         this.moves = moves;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PlayerMoves)) return false;
+        PlayerMoves that = (PlayerMoves) o;
+        if(that.getMoves().items.length != getMoves().items.length) return false;
+        for (int i = 0; i < getMoves().items.length; i++) {
+            if(getMoves().items[i] != that.getMoves().items[i]) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMoves());
+    }
 }
