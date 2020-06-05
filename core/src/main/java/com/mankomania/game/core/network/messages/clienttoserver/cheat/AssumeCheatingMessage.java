976e388558b870685a4bd6f4ae25af5121c07d3e
Fabian Oraze <oraze.fabian@icloud.com>
2020-06-05 10:08:41 +0200

    implemented msg classes for cheating

--- core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/cheat/AssumeCheatingMessage.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/cheat/AssumeCheatingMessage.java
@@ -6,6 +6,20 @@ package com.mankomania.game.core.network.messages.clienttoserver.cheat;
 
 public class AssumeCheatingMessage {
 
+    private int playerIndex;
+
     public AssumeCheatingMessage() {
+    }
+
+    public AssumeCheatingMessage(int playerIndex) {
+        this.playerIndex = playerIndex;
+    }
+
+    public int getPlayerIndex() {
+        return playerIndex;
+    }
+
+    public void setPlayerIndex(int playerIndex) {
+        this.playerIndex = playerIndex;
     }
 }
--- core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/cheat/CheatedMessage.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/cheat/CheatedMessage.java
@@ -6,6 +6,20 @@ package com.mankomania.game.core.network.messages.clienttoserver.cheat;
 
 public class CheatedMessage {
 
+    private int playerIndex;
+
     public CheatedMessage() {
+    }
+
+    public CheatedMessage(int playerIndex) {
+        this.playerIndex = playerIndex;
+    }
+
+    public int getPlayerIndex() {
+        return playerIndex;
+    }
+
+    public void setPlayerIndex(int playerIndex) {
+        this.playerIndex = playerIndex;
     }
 }
