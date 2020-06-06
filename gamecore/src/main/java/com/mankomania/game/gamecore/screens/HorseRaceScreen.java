57c0987bb5add887d07e27f6fba3da151e327d2a
Th3Dilli <dilli.1822@gmx.at>
2020-06-06 19:36:02 +0200

    add HorseRaceSceen

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/HorseRaceScreen.java
@@ -0,0 +1,13 @@
+package com.mankomania.game.gamecore.screens;
+
+public class HorseRaceScreen extends AbstractScreen {
+
+    public HorseRaceScreen() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+}
