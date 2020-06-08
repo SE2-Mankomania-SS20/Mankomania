9cc841ab0acc350796d2de2c4c04e6b227d7e1da
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 03:33:57 +0200

    moved textures and texture regions into its own class, since different row instances will each need those, therefore a data class makes sense.

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotTextures.java
@@ -0,0 +1,47 @@
+package com.mankomania.game.gamecore.screens.slots;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * This class holds all textures and texture regions needed by the slots minigame.
+ */
+public class SlotTextures {
+    private final Texture slotIconTexture;
+
+    // texture regions to specify regions to only render a small rectangle out of the whole texture atlas
+    // each row texture region contains three icons
+    private final TextureRegion slotTextureFirstRow;
+    private final TextureRegion slotTextureSecondRow;
+    private final TextureRegion slotTextureThirdRow;
+    // need to have one specific icon doubled in order to make a smooth, seemless animation
+    private final TextureRegion slotTextureDuplicatIcon;
+
+    public SlotTextures() {
+        // load the slot icon texture containing all icons
+        this.slotIconTexture = new Texture(Gdx.files.local("slot_icons.png"));
+
+        // setting up texture regions: each icon is sized 400x400 pixel, so a row is 400x1200 pixel
+        this.slotTextureFirstRow = new TextureRegion(this.slotIconTexture, 0, 0, 400, 1200);
+        this.slotTextureSecondRow = new TextureRegion(this.slotIconTexture, 400, 0, 400, 1200);
+        this.slotTextureThirdRow = new TextureRegion(this.slotIconTexture, 800, 0, 400, 1200);
+        this.slotTextureDuplicatIcon = new TextureRegion(this.slotIconTexture, 0, 800, 400, 400);
+    }
+
+    public TextureRegion getSlotTextureFirstRow() {
+        return slotTextureFirstRow;
+    }
+
+    public TextureRegion getSlotTextureSecondRow() {
+        return slotTextureSecondRow;
+    }
+
+    public TextureRegion getSlotTextureThirdRow() {
+        return slotTextureThirdRow;
+    }
+
+    public TextureRegion getSlotTextureDuplicatIcon() {
+        return slotTextureDuplicatIcon;
+    }
+}
