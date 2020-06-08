53f7a441d26babb2783f05b0739de619cb6df565
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 12:17:37 +0200

    added another two rolls to the machine and positioned and resized them, so they fit into the 'windows' of the machine nicely.

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotGameActor.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotGameActor.java
@@ -10,22 +10,30 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class SlotGameActor extends Actor {
     private final SlotTextures slotTextures;
     private final SlotIconRow iconRow1;
+    private final SlotIconRow iconRow2;
+    private final SlotIconRow iconRow3;
 
     public SlotGameActor() {
         this.slotTextures = new SlotTextures();
 
         this.iconRow1 = new SlotIconRow(this.slotTextures, 276, 372);
+        this.iconRow2 = new SlotIconRow(this.slotTextures, 640, 372);
+        this.iconRow3 = new SlotIconRow(this.slotTextures, 1004, 372);
     }
 
     @Override
     public void act(float delta) {
         this.iconRow1.update(delta);
+        this.iconRow2.update(delta);
+        this.iconRow3.update(delta);
         super.act(delta);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         this.iconRow1.render(batch);
+        this.iconRow2.render(batch);
+        this.iconRow3.render(batch);
 
         batch.draw(this.slotTextures.getSlotBackgroundTexture(), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
