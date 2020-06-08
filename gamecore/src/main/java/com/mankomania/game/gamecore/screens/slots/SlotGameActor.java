619fe43ca8865231f1e493df4e55902bfb444681
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 03:16:36 +0200

    added Actor to animate and render the 'walzen' and connected the actor with the SlotScreen's mainstage.

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotGameActor.java
@@ -0,0 +1,20 @@
+package com.mankomania.game.gamecore.screens.slots;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * This actor will handle and render the three "Walzen" of the slots minigame.
+ */
+public class SlotGameActor extends Actor {
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotsScreen.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotsScreen.java
@@ -1,18 +1,32 @@
 package com.mankomania.game.gamecore.screens.slots;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mankomania.game.gamecore.screens.AbstractScreen;
 
+/**
+ * Screen that displays the whole Slots minigame.
+ */
 public class SlotsScreen extends AbstractScreen {
+    private Stage mainStage;
+
     public SlotsScreen() {
+        this.mainStage = new Stage();
+
+        SlotGameActor slotActor = new SlotGameActor();
+        slotActor.setPosition(300f, 300f);
+        slotActor.setSize(600f, 600);
+
+        this.mainStage.addActor(slotActor);
     }
 
     @Override
     public void render(float delta) {
-        super.render(delta);
+        this.mainStage.act(delta);
+        this.mainStage.draw();
     }
 
     @Override
     public void dispose() {
-        super.dispose();
+        this.mainStage.dispose();
     }
 }
