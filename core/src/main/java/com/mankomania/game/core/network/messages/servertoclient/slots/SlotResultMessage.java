4b8fa05319db093a757961d2db4d6532724b59b8
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 20:47:37 +0200

    added and implemented SlotResultMessage class.

---
+++ core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/slots/SlotResultMessage.java
@@ -0,0 +1,32 @@
+package com.mankomania.game.core.network.messages.servertoclient.slots;
+
+/**
+ * This message gets sent from the server to the client after the client has sent his SpinRollsMessage.
+ * It contains the spin result.
+ */
+public class SlotResultMessage {
+    private int playerIndex;
+
+    /**
+     * storing the roll result, rolls icons are between 0 and 8 (9 different icons)
+     * the array has a length of 3, since there are three separate rolls
+     */
+    private int[] rollResult;
+
+    public SlotResultMessage() {
+        // empty ctor needed for kryonet deserialization
+    }
+
+    public SlotResultMessage(int playerIndex, int[] rollResult) {
+        this.playerIndex = playerIndex;
+        this.rollResult = rollResult;
+    }
+
+    public int getPlayerIndex() {
+        return playerIndex;
+    }
+
+    public int[] getRollResult() {
+        return rollResult;
+    }
+}
