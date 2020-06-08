41bf711ef422581cbc0241502e6939f9566af672
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 20:47:15 +0200

    added and implemented SpinRollsMessage class.

---
+++ core/src/main/java/com/mankomania/game/core/network/messages/clienttoserver/slots/SpinRollsMessage.java
@@ -0,0 +1,8 @@
+package com.mankomania.game.core.network.messages.clienttoserver.slots;
+
+/**
+ * This message gets sent from the client to the server if the player touches the lever.
+ * The server then will generate the roll results and send it to the players as a SlotResultMessage.
+ */
+public class SpinRollsMessage {
+}
--- core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/slots/StartSlotsMessage.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/slots/StartSlotsMessage.java
@@ -2,15 +2,16 @@ package com.mankomania.game.core.network.messages.servertoclient.slots;
 
 /**
  * This message gets sent from the server to the client, when a player moves over (or onto) the casino minigame field.
+ * If this message gets received at the clients, the clients open up the slot minigame screen.
  */
-public class StartSlotsMsg {
+public class StartSlotsMessage {
     private int playerIndex;
 
-    public StartSlotsMsg() {
+    public StartSlotsMessage() {
         // empty ctor to enable kryonet deserialization
     }
 
-    public StartSlotsMsg(int playerIndex) {
+    public StartSlotsMessage(int playerIndex) {
         this.playerIndex = playerIndex;
     }
 
