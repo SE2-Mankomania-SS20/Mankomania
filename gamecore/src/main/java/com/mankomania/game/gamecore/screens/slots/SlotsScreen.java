0fc6619310e66d749795d4392b3559ecb3d31612
Felix Mitterer <felix.mitterer@posteo.de>
2020-06-08 03:06:27 +0200

    added basic screen class for the slots minigame UI.

---
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/slots/SlotsScreen.java
@@ -0,0 +1,18 @@
+package com.mankomania.game.gamecore.screens.slots;
+
+import com.mankomania.game.gamecore.screens.AbstractScreen;
+
+public class SlotsScreen extends AbstractScreen {
+    public SlotsScreen() {
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
