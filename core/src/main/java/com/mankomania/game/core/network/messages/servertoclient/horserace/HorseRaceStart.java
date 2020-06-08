e1c4cfbc6a06be78f1d6115b6352fbb19e215626
Th3Dilli <dilli.1822@gmx.at>
2020-06-08 23:47:43 +0200

    add horse race messages

--- core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/horserace/HorseRaceStart.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/horserace/HorseRaceStart.java
@@ -1,5 +1,17 @@
 package com.mankomania.game.core.network.messages.servertoclient.horserace;
 
 public class HorseRaceStart {
+    private int currentPlayerIndex;
 
+    public HorseRaceStart() {
+        // empty for kryonet
+    }
+
+    public HorseRaceStart(int currentPlayerIndex) {
+        this.currentPlayerIndex = currentPlayerIndex;
+    }
+
+    public int getCurrentPlayerIndex() {
+        return currentPlayerIndex;
+    }
 }
--- core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/horserace/HorseRaceUpdate.java
+++ core/src/main/java/com/mankomania/game/core/network/messages/servertoclient/horserace/HorseRaceUpdate.java
@@ -1,4 +1,31 @@
 package com.mankomania.game.core.network.messages.servertoclient.horserace;
 
+import com.mankomania.game.core.data.horserace.HorseRaceData;
+import com.mankomania.game.core.data.horserace.HorseRacePlayerInfo;
+
+import java.util.List;
+
+
 public class HorseRaceUpdate {
+
+    private int currentPlayerIndex;
+
+    private List<HorseRacePlayerInfo> horseRacePlayerInfos;
+
+    public HorseRaceUpdate() {
+        // empty for kryonet
+    }
+
+    public HorseRaceUpdate(HorseRaceData horseRaceData) {
+        horseRacePlayerInfos = horseRaceData.getHorseRacePlayerInfo();
+        currentPlayerIndex = horseRaceData.getCurrentPlayerIndex();
+    }
+
+    public List<HorseRacePlayerInfo> getHorseRacePlayerInfos() {
+        return horseRacePlayerInfos;
+    }
+
+    public int getCurrentPlayerIndex() {
+        return currentPlayerIndex;
+    }
 }
