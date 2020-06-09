9fdf353ead658464ef9a1424d094813e70b0e1b8
Fabian Oraze <oraze.fabian@icloud.com>
2020-06-09 09:38:15 +0200

    removed unused import

--- gamecore/src/main/java/com/mankomania/game/gamecore/client/MessageHandler.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/client/MessageHandler.java
@@ -21,7 +21,6 @@ import com.mankomania.game.core.network.messages.clienttoserver.stock.StockResul
 import com.mankomania.game.core.network.messages.clienttoserver.trickyone.RollDiceTrickyOne;
 import com.mankomania.game.core.network.messages.clienttoserver.trickyone.StopRollingDice;
 import com.mankomania.game.core.network.messages.servertoclient.trickyone.CanRollDiceTrickyOne;
-import com.mankomania.game.core.network.messages.servertoclient.trickyone.EndTrickyOne;
 import com.mankomania.game.core.network.messages.servertoclient.hotel.PlayerBoughtHotelMessage;
 import com.mankomania.game.core.network.messages.servertoclient.hotel.PlayerCanBuyHotelMessage;
 import com.mankomania.game.core.network.messages.servertoclient.hotel.PlayerPaysHotelRentMessage;
