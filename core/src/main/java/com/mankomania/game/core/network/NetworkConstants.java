d2daf8577c7fe15e5e0501cf0a6c4b31ecf7115f
Ajdin Memić <ajdinmemic99@outlook.com>
2020-06-13 15:33:11 +0200

    Network Constants

--- core/src/main/java/com/mankomania/game/core/network/NetworkConstants.java
+++ core/src/main/java/com/mankomania/game/core/network/NetworkConstants.java
@@ -9,6 +9,6 @@ public class NetworkConstants {
     }
 
     public static final int TCP_PORT = 53211;
-    public static final String IP_HOST = "192.168.0.101";
+    public static final String IP_HOST = "se2-demo.aau.at";
     public static final int TIMEOUT = 5000;
 }
