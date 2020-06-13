68bbe3739360f21773fe0b1cd3ff04359f6b6457
Ajdin Memić <ajdinmemic99@outlook.com>
2020-06-14 00:14:20 +0200

    AktienBoerse refactored

--- gamecore/src/main/java/com/mankomania/game/gamecore/screens/stockexchange/AktienBoerse.java
+++ gamecore/src/main/java/com/mankomania/game/gamecore/screens/stockexchange/AktienBoerse.java
@@ -9,12 +9,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Timer;
+import com.mankomania.game.core.data.GameData;
+import com.mankomania.game.core.player.Stock;
 import com.mankomania.game.gamecore.MankomaniaGame;
 import com.mankomania.game.gamecore.screens.AbstractScreen;
 import com.mankomania.game.gamecore.util.AssetPaths;
-import com.mankomania.game.gamecore.util.Screen;
-import com.mankomania.game.gamecore.util.ScreenManager;
 
 public class AktienBoerse extends AbstractScreen {
     private Stage stage;
@@ -25,10 +24,10 @@ public class AktienBoerse extends AbstractScreen {
     private Skin skin;
     private Label rolltext;
     private int inputCount = 0;
-    String defaultStyle="default";
+    String defaultStyle = "black";
 
     public AktienBoerse() {
-        String input="Input";
+        String input = "Input";
 
         skin = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.SKIN);
         stage = new Stage();
@@ -41,7 +40,7 @@ public class AktienBoerse extends AbstractScreen {
         Gdx.input.setInputProcessor(stage);
         skin.getFont("font").getData().setScale(4, 4);
 
-        walze = (MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNEN));
+        walze = (MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GELD_PNG));
         walzeimage = new Image(walze);
         rolltext = new Label("Roll it!", skin, defaultStyle);
         resultat = new Label("Outcome:", skin, defaultStyle);
@@ -52,38 +51,39 @@ public class AktienBoerse extends AbstractScreen {
         table.add(resultat).padTop(50);
         walzeimage.setPosition(0, 0);
         stage.addActor(table);
+        if (MankomaniaGame.getMankomaniaGame().isLocalPlayerTurn()) {
+            Gdx.input.setInputProcessor(new SwipeDetector(new SwipeDetector.DirectionListener() {
 
-        Gdx.input.setInputProcessor(new SwipeDetector(new SwipeDetector.DirectionListener() {
-
-            @Override
-            public void onUp() {
-                if (inputCount == 0) {
-                    result();
-                    inputCount++;
+                @Override
+                public void onUp() {
+                    if (inputCount == 0) {
+                        result();
+                        inputCount++;
+                    }
+                    Gdx.app.log(input, "Up");
                 }
-                Gdx.app.log(input, "Up");
-            }
 
 
-            @Override
-            public void onRight() {
-                Gdx.app.log(input, "Right (Methode soll nicht gecalled werden)");
-            }
-
-            @Override
-            public void onLeft() {
-                Gdx.app.log(input, "Left (Methode soll nicht gecalled werden)");
-            }
-
-            @Override
-            public void onDown() {
-                if (inputCount == 0) {
-                    result();
-                    inputCount++;
+                @Override
+                public void onRight() {
+                    Gdx.app.log(input, "Right (Methode soll nicht gecalled werden)");
                 }
-                Gdx.app.log(input, "Down");
-            }
-        }));
+
+                @Override
+                public void onLeft() {
+                    Gdx.app.log(input, "Left (Methode soll nicht gecalled werden)");
+                }
+
+                @Override
+                public void onDown() {
+                    if (inputCount == 0) {
+                        result();
+                        inputCount++;
+                    }
+                    Gdx.app.log(input, "Down");
+                }
+            }));
+        }
     }
 
     @Override
@@ -91,83 +91,90 @@ public class AktienBoerse extends AbstractScreen {
         super.render(delta);
         stage.act(delta);
         stage.draw();
+        update();
         super.renderNotifications(delta);
     }
 
     private void result() {
-
         int max = 6;
         int min = 1;
         int range = max - min + 1;
         int random = (int) (Math.random() * range) + min;
-        Label text = new Label("", skin);
-        Label text2 = new Label("", skin);
-        Label text3 = new Label("", skin);
-        Label text4 = new Label("", skin);
-        table.clear();
-
-        Texture walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNEN);
-        walzeimage = new Image(walzeZwei);
-        rolltext = new Label("Roll it!", skin, defaultStyle);
-        resultat = new Label("Outcome:", skin, defaultStyle);
-
         MankomaniaGame.getMankomaniaGame().getNetworkClient().getMessageHandler().sendStockResultMessage(random);
-        if (random == 1 || random == 4) {
-            text.setText("Jeder mit Bruchstahl Aktien");
-        }
-        if (random == 2 || random == 5) {
-            text.setText("Jeder mit Kurzschluss Aktien");
-        }
-        if (random == 3 || random == 6) {
-            text.setText("Jeder mit Trockenoel Aktien");
-        }
-        if (random == 1 || random == 2 || random == 3) {
-            if(random == 1){walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENB);
-            walzeimage = new Image(walzeZwei);}
-            else if(random == 2){walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENK);
-            walzeimage = new Image(walzeZwei);}
-            else {walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENT);
-            walzeimage = new Image(walzeZwei);}
-            text2.setText("bekommt");
-            text3.setText("+10.000");
-            text4 = new Label("pro Aktie von der Bank", skin,  defaultStyle);
-        }
-        if (random == 4 || random == 5 || random == 6) {
-            if(random == 1){walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENB);
-            walzeimage = new Image(walzeZwei);}
-            else if(random == 2){walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENK);
-            walzeimage = new Image(walzeZwei);}
-            else {walzeZwei=MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENT);
-            walzeimage = new Image(walzeZwei);}
-            text2.setText("verliert");
-            text3 = new Label("-10.000", skin, "red");
-            text4.setText("pro Aktie");
-        }
-
-        table.add(rolltext).padTop(50);
-        table.row();
-        table.add(walzeimage).padTop(50).width(Gdx.graphics.getWidth() / 2f);
-        table.row();
-        table.add(resultat).padTop(50);
-        table.row();
-        table.add(text).padTop(25);
-        table.row();
-        table.add(text2);
-        table.row();
-        table.add(text3);
-        table.row();
-        table.add(text4);
-
-        backToMainGame();
     }
 
-    private void backToMainGame() {
-        float delayInSeconds = 3f;
-        Timer.schedule(new Timer.Task() {
-            @Override
-            public void run() {
-                ScreenManager.getInstance().switchScreen(Screen.MAIN_GAME);
+    public void update() {
+        GameData refGameData = MankomaniaGame.getMankomaniaGame().getGameData();
+        if (refGameData.getAktienBoerseData().isNeedUpdate()) {
+            refGameData.getAktienBoerseData().setNeedUpdate(false);
+            Stock stock = refGameData.getAktienBoerseData().getStock();
+            boolean isRising = refGameData.getAktienBoerseData().isRising();
+            Label text = new Label("", skin, defaultStyle);
+            Label text2 = new Label("", skin, defaultStyle);
+            Label text3 = new Label("", skin, defaultStyle);
+            Label text4 = new Label("", skin, defaultStyle);
+            table.clear();
+
+            Texture walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GELD_PNG);
+            walzeimage = new Image(walzeZwei);
+            rolltext = new Label("Roll it!", skin, defaultStyle);
+            resultat = new Label("Outcome:", skin, defaultStyle);
+
+            switch (stock) {
+                case BRUCHSTAHLAG: {
+                    text.setText("Jeder mit Bruchstahl Aktien");
+                    if(isRising){
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENB);
+                    }else {
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENB);
+                    }
+                    break;
+                }
+                case KURZSCHLUSSAG: {
+                    text.setText("Jeder mit Kurzschluss Aktien");
+                    if(isRising){
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENK);
+                    }else {
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENK);
+                    }
+                    break;
+                }
+                case TROCKENOEL: {
+                    text.setText("Jeder mit Trockenoel Aktien");
+                    if(isRising){
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.GEWONNENT);
+                    }else {
+                        walzeZwei = MankomaniaGame.getMankomaniaGame().getManager().get(AssetPaths.VERLORENT);
+                    }
+                    break;
+                }
             }
-        }, delayInSeconds);
+
+            if (isRising) {
+                text2.setText("bekommt");
+                text3.setText("+10.000");
+                text4 = new Label("pro Aktie von der Bank", skin, defaultStyle);
+            } else {
+                text2.setText("verliert");
+                text3 = new Label("-10.000", skin, "red");
+                text4.setText("pro Aktie");
+            }
+
+            walzeimage = new Image(walzeZwei);
+
+            table.add(rolltext).padTop(50);
+            table.row();
+            table.add(walzeimage).padTop(50).width(Gdx.graphics.getWidth() / 2f);
+            table.row();
+            table.add(resultat).padTop(50);
+            table.row();
+            table.add(text).padTop(25);
+            table.row();
+            table.add(text2);
+            table.row();
+            table.add(text3);
+            table.row();
+            table.add(text4);
+        }
     }
 }
